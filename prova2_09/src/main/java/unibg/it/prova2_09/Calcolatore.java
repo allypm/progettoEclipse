de4538d9d9bee5360eb8abcdef0ba22895be9ed4
allypm <a.perezmurillo@studenti.unibg.it>
2025-09-25 17:27:09 +0200

    Update Calcolatore.java

--- prova2_09/src/main/java/unibg/it/prova2_09/Calcolatore.java
+++ prova2_09/src/main/java/unibg/it/prova2_09/Calcolatore.java
@@ -1,10 +1,19 @@
 package unibg.it.prova2_09;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class Calcolatore {
+	
+	public class Calculator {
+		int x, y;
+		
+		}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
 	}
 
 }
