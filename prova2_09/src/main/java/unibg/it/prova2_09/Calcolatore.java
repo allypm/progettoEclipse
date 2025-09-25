ae4c99712309118d45c38f80896ab74cd61c0145
F-Bucci <f.bucci1@studenti.unibg.it>
2025-09-25 17:41:49 +0200

    Update Calcolatore.java

--- prova2_09/src/main/java/unibg/it/prova2_09/Calcolatore.java
+++ prova2_09/src/main/java/unibg/it/prova2_09/Calcolatore.java
@@ -3,14 +3,14 @@ package unibg.it.prova2_09;
 
 public class Calcolatore {
 	
-	public class Calculator {
-		int x, y;
-		
-		}
-
-	public static void main(String[] args) {
-		
-		
+		int number1;
+		int number2;
+	public Calculator (int number1, int number2) {
+		this.number1=number1;
+		this.number2=number2;
 	}
-
-}
+	public int Sum () {
+		return (this.number1 + this.number2);
+	}
+	}
+	
